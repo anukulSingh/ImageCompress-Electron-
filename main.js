7c0ccd287dea6f6a6723a104d6cb13a19112f1bd
Anukul kumar <anukulsingh125@gmail.com>
2020-11-03 23:58:57 +0530

    application built, ready for production

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const path = require('path');
+const os = require('os')
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
+const log = require('electron-log')
+
 
 //set env
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
@@ -109,8 +117,32 @@ const menu = [
 // }
 
 ipcMain.on('image:minimize', (e, options) => {
-    console.log(options);
+    options.dest = path.join(os.homedir(), 'imagecompress')
+    shrinkImage(options);
 })
+
+async function shrinkImage({ imgPath, quality, dest }) {
+    try {
+        const pngQuality = quality / 100
+
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest,
+            plugins: [
+                imageminMozjpeg({ quality }),
+                imageminPngquant({
+                    quality: [pngQuality, pngQuality]
+                })
+            ]
+        })
+        log.info(files)
+
+        shell.openPath(dest)
+
+        mainWindow.webContents.send('image:done')
+    } catch (error) {
+        log.error(error);
+    }
+}
 
 app.on('window-all-closed', () => {
     if(isMac) {
